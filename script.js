52871bad2d8f64bdf1769685c2e9548af2e56f74
Brandon <brandon.messager@gmail.com>
2022-07-19 12:24:22 +0200

    debut netoyage

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
-let score = 0;
+let score = 100000000000;
 let clickMultiplier = 1;
 let click = 1;
-let multiplyPrice = 50;
-let wingedPrice = 200;
-let kuribohPrice = 50;
-let kuribeePrice = 3200;
-let kuribooPrice = 400;
-let kuribahPrice = 204800;
-let kuribehPrice = 25600;
+let price = {
+            multiply:50,
+            kuriboh:50,
+            winged:200,
+            kuriboo:400,
+            kuribee:3200,
+            kuribeh:25600,
+            kuribah:204800};
 let wingedMultiplier = 1;
-let wingedlvl = 0;
-let kuribohlvl = 0;
-let kuriboolvl = 0;
-let kuribeelvl = 0;
-let kuribahlvl = 0;
-let kuribehlvl = 0;
-let babylonlvl = 0;
-let banditlvl = 0;
+let lvl = {
+            kuriboh:0,
+            winged:0,
+            kuriboo:0,
+            kuribee:0,
+            kuribeh:0,
+            kuribah:0,
+            babylon:0,
+            bandit:0};
 let wingedAutoAcquired = false;
 let wing10acquired = false;
 let addBtn = document.getElementById("click");
@@ -33,8 +35,8 @@ let kuribeh = document.getElementById("kuribeh");
 let kuribee = document.getElementById("kuribee");
 let stars = document.getElementById("stars");
 let container = document.getElementById("falling");
-let containerWidth = container.clientWidth;
-let containerHeight = container.clientHeight;
+const containerWidth = container.clientWidth;
+const containerHeight = container.clientHeight;
 let idnumber = 0;
 let getid = false;
 
@@ -53,8 +55,20 @@ kuriboo.onclick = addkuriboo;
 function addpoint()
 {
     score += click*clickMultiplier;
-    scoreDisplay.innerHTML = 'kuriboh: ' + score;
     fallingkuriboh();
+    displayScore();
+}
+
+function displayScore(){
+    scoreDisplay.innerHTML = 'kuriboh: ' + score;
+}
+
+function displayKpc(){
+    clickDisplay.innerHTML = 'kuriboh/click: ' + click*clickMultiplier;
+}
+
+function displayKps(){
+    dpsDisplay.innerHTML = "kuriboh/s: " + (wingedMultiplier + lvl['bandit']*15);
 }
 
 function fallingkuriboh()
@@ -69,7 +83,6 @@ function fallingkuriboh()
 function positionElement()
 {
     let sideContainer = document.getElementById("sideContainer");
-    let sideContainerWidth = sideContainer.clientWidth;
     let setLeftPos = Math.floor(Math.random() * (containerWidth-100));
     let Currentdiv = document.getElementById(idnumber + "fallingKuriboh");
     Currentdiv.style.position = "absolute";
@@ -102,47 +115,47 @@ function positionElement()
 
 
 function addmultiply(){
-    if(score>=multiplyPrice){
-        score -= multiplyPrice;
-        multiplyPrice *= 2;
-        scoreDisplay.innerHTML = 'kuriboh: ' + score;
+    if(score>=price['multiply']){
+        score -= price['multiply'];
+        price['multiply'] *= 2;
+        displayScore();
         clickMultiplier++;
-        clickDisplay.innerHTML = 'kuriboh/click: ' + click*clickMultiplier;
-        document.getElementById("multiplyCost").innerHTML = "Multiplicateur </br>x" + clickMultiplier + "</br>coût: " + multiplyPrice + " kuriboh";
+        displayKpc();
+        document.getElementById("multiplyCost").innerHTML = "Multiplicateur </br>x" + clickMultiplier + "</br>coût: " + price['multiply'] + " kuriboh";
     }
 }
 
 function addwinged(){
-    if(score>=wingedPrice && wingedlvl<9){
-        score -= wingedPrice;
-        wingedlvl ++;
-        wingedPrice *= 2;
-        scoreDisplay.innerHTML = 'kuriboh: ' + score;
+    if(score>=price['winged'] && lvl['winged']<9){
+        score -= price['multiply'];
+        lvl['winged'] ++;
+        price['multiply'] *= 2;
+        displayScore(score);
         wingedMultiplier += wingedMultiplier;
         if(wingedAutoAcquired == false){
             wingedAutoAcquired = true;
             setInterval(Autowinged,1000);
         }
-        dpsDisplay.innerHTML = "kuriboh/s: " + (wingedMultiplier + banditlvl*15); 
-        if(wingedlvl == 9){
+        displayKps(); 
+        if(lvl['winged'] == 9){
             document.getElementById("wingedImage").classList.replace('winged','winged9');
-            document.getElementById("wingsCost").innerHTML = "Ailes transcendantes</br>cost: " + wingedPrice;
+            document.getElementById("wingsCost").innerHTML = "Ailes transcendantes</br>cost: " + price['multiply'];
             document.getElementById("wingedClickable").classList.remove('clickable');
             wings.classList.remove('hidden');
-            document.getElementById("wingedCost").innerHTML = "Kuriboh ailée (lvl " + wingedlvl + ")</br>"+ wingedMultiplier + " kuriboh/s";
+            document.getElementById("wingedCost").innerHTML = "Kuriboh ailée (lvl " + lvl['winged'] + ")</br>"+ wingedMultiplier + " kuriboh/s";
         }else{
-            document.getElementById("wingedCost").innerHTML = "Kuriboh ailée (lvl " + wingedlvl + ")</br>"+ wingedMultiplier + " kuriboh/s</br>coût: " + wingedPrice + " kuriboh";
+            document.getElementById("wingedCost").innerHTML = "Kuriboh ailée (lvl " + lvl['winged'] + ")</br>"+ wingedMultiplier + " kuriboh/s</br>coût: " + price['multiply'] + " kuriboh";
         }
     }
 }
 
 function Autowinged(){
-    score += wingedMultiplier + banditlvl*15;
-    scoreDisplay.innerHTML = 'kuriboh: ' + score;
+    score += wingedMultiplier + lvl['bandit']*15;
+    displayScore();
 }
 
 function addwing10(){
-    if(score>=wingedPrice && wing10acquired == false){
+    if(score>=price['multiply'] && wing10acquired == false){
         wingedMultiplier *= 10;
         dpsDisplay.innerHTML = "kuriboh/s: " + wingedMultiplier;
         wing10acquired = true;
@@ -156,129 +169,129 @@ function addwing10(){
 function addClick(){
     click += 1;
     clickDisplay.innerHTML = 'kuriboh/clicks: ' + click*clickMultiplier;
-    scoreDisplay.innerHTML = 'kuriboh: ' + score;
+    displayScore(score);
 }
 
 function addkuriboh(){
-    if(score>=kuribohPrice && kuribohlvl<3){
-        kuribohlvl ++;
-        score -= kuribohPrice;
-        kuribohPrice *= 2;
+    if(score>=price['kuriboh'] && lvl['kuriboh']<3){
+        lvl['kuriboh'] ++;
+        score -= price['kuriboh'];
+        price['kuriboh'] *= 2;
         addClick();
-        if(kuribohlvl == 3){
+        if(lvl['kuriboh'] == 3){
             document.getElementById("kuribohClickable").classList.remove('clickable');
-            document.getElementById("kuribohCost").innerHTML = "Kuriboh x" + kuribohlvl + " max";
+            document.getElementById("kuribohCost").innerHTML = "Kuriboh x" + lvl['kuriboh'] + " max";
             kuriboo.classList.remove('hidden');
-            document.getElementById("kuribooCost").innerHTML = "Kuriboo x" + kuriboolvl + "</br>coût: " +  kuribooPrice + " kuriboh";
+            document.getElementById("kuribooCost").innerHTML = "Kuriboo x" + lvl['kuriboo'] + "</br>coût: " +  price['kuriboo'] + " kuriboh";
         }else{
-            document.getElementById("kuribohCost").innerHTML = "Kuriboh x" + kuribohlvl + "</br>coût: " +  kuribohPrice + " kuriboh";
+            document.getElementById("kuribohCost").innerHTML = "Kuriboh x" + lvl['kuriboh'] + "</br>coût: " +  price['kuriboh'] + " kuriboh";
         }
     }
 }
 
 function addkuriboo(){
-    if(score>=kuribooPrice && kuriboolvl<3){
-        kuriboolvl ++;
-        score -= kuribooPrice;
-        kuribooPrice *=2
+    if(score>=price['kuriboo'] && lvl['kuriboo']<3){
+        lvl['kuriboo'] ++;
+        score -= price['kuriboo'];
+        price['kuriboo'] *=2
         addClick();
-        if(kuriboolvl == 3){
+        if(lvl['kuriboo'] == 3){
             document.getElementById("kuribooClickable").classList.remove('clickable');
-            document.getElementById("kuribooCost").innerHTML = "Kuriboo x" + kuriboolvl + " max";
+            document.getElementById("kuribooCost").innerHTML = "Kuriboo x" + lvl['kuriboo'] + " max";
             kuribee.classList.remove('hidden');
-            document.getElementById("kuribeeCost").innerHTML = "Kuribee x" + kuribeelvl + "</br>coût: " +  kuribeePrice + " kuriboh";
+            document.getElementById("kuribeeCost").innerHTML = "Kuribee x" + lvl['kuribee'] + "</br>coût: " +  price['kuribee'] + " kuriboh";
         }else{
-            document.getElementById("kuribooCost").innerHTML = "Kuriboo x" + kuriboolvl + "</br>coût: " +  kuribooPrice + " kuriboh";
+            document.getElementById("kuribooCost").innerHTML = "Kuriboo x" + lvl['kuriboo'] + "</br>coût: " +  price['kuriboo'] + " kuriboh";
         }
     }
 }
 
 function addkuribee(){
-    if(score>=kuribeePrice && kuribeelvl<3){
-        kuribeelvl ++;
-        score -= kuribeePrice;
-        kuribeePrice *= 2;
+    if(score>=price['kuribee'] && lvl['kuribee']<3){
+        lvl['kuribee'] ++;
+        score -= price['kuribee'];
+        price['kuribee'] *= 2;
         addClick();
-        if(kuribeelvl == 3){
+        if(lvl['kuribee'] == 3){
             document.getElementById("kuribeeClickable").classList.remove('clickable');
-            document.getElementById("kuribeeCost").innerHTML = "Kuribee x" + kuribeelvl + " max";
+            document.getElementById("kuribeeCost").innerHTML = "Kuribee x" + lvl['kuribee'] + " max";
             kuribeh.classList.remove('hidden');
-            document.getElementById("kuribehCost").innerHTML = "Kuribeh x" + kuribehlvl + "</br>coût: " +  kuribehPrice + " kuriboh";
+            document.getElementById("kuribehCost").innerHTML = "Kuribeh x" + lvl['kuribeh'] + "</br>coût: " +  price['kuribeh'] + " kuriboh";
         }else{
-        document.getElementById("kuribeeCost").innerHTML = "Kuribee x" + kuribeelvl + "</br>coût: " +  kuribeePrice + " kuriboh";
+        document.getElementById("kuribeeCost").innerHTML = "Kuribee x" + lvl['kuribee'] + "</br>coût: " +  price['kuribee'] + " kuriboh";
         }
     }
 }
 
 function addkuribeh(){
-    if(score>=kuribehPrice && kuribehlvl<3){
-        kuribehlvl ++;
-        score -= kuribehPrice;
-        kuribehPrice *= 2;
+    if(score>=price['kuribeh'] && lvl['kuribeh']<3){
+        lvl['kuribeh'] ++;
+        score -= price['kuribeh'];
+        price['kuribeh'] *= 2;
         addClick();
-        if(kuribehlvl == 3){
+        if(lvl['kuribeh'] == 3){
             document.getElementById("kuribehClickable").classList.remove('clickable');
-            document.getElementById("kuribehCost").innerHTML = "Kuribeh x" + kuribehlvl + " max";
+            document.getElementById("kuribehCost").innerHTML = "Kuribeh x" + lvl['kuribeh'] + " max";
             kuribah.classList.remove('hidden');
-            document.getElementById("kuribahCost").innerHTML = "Kuribah x" + kuribahlvl + "</br>coût: " +  kuribahPrice + " kuriboh";
+            document.getElementById("kuribahCost").innerHTML = "Kuribah x" + lvl['kuribah'] + "</br>coût: " +  price['kuribah'] + " kuriboh";
         }else{
-        document.getElementById("kuribehCost").innerHTML = "Kuribeh x" + kuribehlvl + "</br>coût: " +  kuribehPrice + " kuriboh";
+        document.getElementById("kuribehCost").innerHTML = "Kuribeh x" + lvl['kuribeh'] + "</br>coût: " +  price['kuribeh'] + " kuriboh";
         }
     }
 }
 
 function addkuribah(){
-    if(score>=kuribahPrice && kuribahlvl<3){
-        kuribahlvl ++;
-        score -= kuribahPrice;
-        kuribahPrice *= 2;
+    if(score>=price['kuribah'] && lvl['kuribah']<3){
+        lvl['kuribah'] ++;
+        score -= price['kuribah'];
+        price['kuribah'] *= 2;
         addClick();
-        if(kuribahlvl == 3){
+        if(lvl['kuribah'] == 3){
             document.getElementById("kuribahClickable").classList.remove('clickable');
-            document.getElementById("kuribahCost").innerHTML = "Kuribah x" + kuribahlvl + " max";
-            if(babylonlvl == 0){
+            document.getElementById("kuribahCost").innerHTML = "Kuribah x" + lvl['kuribah'] + " max";
+            if(lvl['babylon'] == 0){
                 stars.classList.remove('hidden');
             }
         }else{
-            document.getElementById("kuribahCost").innerHTML = "Kuribah x" + kuribahlvl + "</br>coût: " +  kuribahPrice + " kuriboh";
+            document.getElementById("kuribahCost").innerHTML = "Kuribah x" + lvl['kuribah'] + "</br>coût: " +  price['kuribah'] + " kuriboh";
         }
     }
 }
 
 function addstars(){
-    if(kuribohlvl == 3 && kuribeelvl == 3 && kuribahlvl == 3 && kuriboolvl == 3 && kuribehlvl == 3){
+    if(lvl['kuriboh'] == 3 && lvl['kuribee'] == 3 && lvl['kuribah'] == 3 && lvl['kuriboo'] == 3 && lvl['kuribeh'] == 3){
         let r = Math.random();
         if(r < 0.5){
             document.getElementById("kuribabylon").classList.remove('hidden');
-            babylonlvl ++;
+            lvl['babylon'] ++;
             click -= 10;
-            document.getElementById("kuribabylonCost").innerHTML = "Kuribabylon x" + babylonlvl + "</br>kuriboh/click: " + babylonlvl*5;
+            document.getElementById("kuribabylonCost").innerHTML = "Kuribabylon x" + lvl['babylon'] + "</br>kuriboh/click: " + lvl['babylon']*5;
         }else{
             document.getElementById("kuribandit").classList.remove('hidden');
-            banditlvl ++;
+            lvl['bandit'] ++;
             click -=15;
             if(wingedAutoAcquired == false){
                 wingedAutoAcquired = true;
                 setInterval(Autowinged,1000);
             }
-            dpsDisplay.innerHTML = "kuriboh/s: " + (wingedMultiplier + banditlvl*15);
-            document.getElementById("kuribanditCost").innerHTML = "Kuribandit x" + banditlvl + "</br>kuriboh/s: " + banditlvl*15;
+            displayKps();
+            document.getElementById("kuribanditCost").innerHTML = "Kuribandit x" + lvl['bandit'] + "</br>kuriboh/s: " + lvl['bandit']*15;
         }
-        kuribohlvl = 0;
-        kuribeelvl = 0;
-        kuriboolvl = 0;
-        kuribehlvl = 0;
-        kuribahlvl = 0;
+        lvl['kuriboh'] = 0;
+        lvl['kuribee'] = 0;
+        lvl['kuriboo'] = 0;
+        lvl['kuribeh'] = 0;
+        lvl['kuribah'] = 0;
         document.getElementById("kuribohClickable").classList.add('clickable');
         document.getElementById("kuribooClickable").classList.add('clickable');
         document.getElementById("kuribeeClickable").classList.add('clickable');
         document.getElementById("kuribehClickable").classList.add('clickable');
         document.getElementById("kuribahClickable").classList.add('clickable');
         clickDisplay.innerHTML = 'kuriboh/clicks: ' + click*clickMultiplier;
-        document.getElementById("kuribohCost").innerHTML = "Kuriboh x" + kuribohlvl + "</br>coût: " +  kuribohPrice + " kuriboh";
-        document.getElementById("kuribooCost").innerHTML = "Kuriboo x" + kuriboolvl + "</br>coût: " +  kuribooPrice + " kuriboh";
-        document.getElementById("kuribeeCost").innerHTML = "Kuribee x" + kuribeelvl + "</br>coût: " +  kuribeePrice + " kuriboh";
-        document.getElementById("kuribehCost").innerHTML = "Kuribeh x" + kuribehlvl + "</br>coût: " +  kuribehPrice + " kuriboh";
-        document.getElementById("kuribahCost").innerHTML = "Kuribah x" + kuribahlvl + "</br>coût: " +  kuribahPrice + " kuriboh";
+        document.getElementById("kuribohCost").innerHTML = "Kuriboh x" + lvl['kuriboh'] + "</br>coût: " +  price['kuriboh'] + " kuriboh";
+        document.getElementById("kuribooCost").innerHTML = "Kuriboo x" + lvl['kuriboo'] + "</br>coût: " +  price['kuriboo'] + " kuriboh";
+        document.getElementById("kuribeeCost").innerHTML = "Kuribee x" + lvl['kuribee'] + "</br>coût: " +  price['kuribee'] + " kuriboh";
+        document.getElementById("kuribehCost").innerHTML = "Kuribeh x" + lvl['kuribeh'] + "</br>coût: " +  price['kuribeh'] + " kuriboh";
+        document.getElementById("kuribahCost").innerHTML = "Kuribah x" + lvl['kuribah'] + "</br>coût: " +  price['kuribah'] + " kuriboh";
     }
 }
